b069eeddec7a66e7fdbc9d6a4255ed378aa2f299
devkid <skhaleed180@gmail.com>
2025-11-22 20:36:05 +0100

    Added contact, admin, CRUD features

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const db = require("../models/db");
 
 router.get("/admin", async (req, res) => {
-  const [users] = await db.query("SELECT id, username, email FROM users");
-  res.render("admin", { users });
+  try {
+    const [users] = await db.query("SELECT id, username, email FROM users");
+    res.render("admin", { users });
+  } catch (err) {
+    res.send("Error: " + err.message);
+  }
 });
 
 module.exports = router;
